7db9f61e18e89ae0f8288a73e1b8159a33c10fb8
Mikhail Libchenko <libchenkomikhail@gmail.com>
2025-05-26 08:45:53 +0300

    feat: update profile moved to a separate component

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,19 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { AuthService } from '@core/auth.service';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { NgIf, NgOptimizedImage } from '@angular/common';
-import { MatDivider } from '@angular/material/divider';
 import { FormsModule } from '@angular/forms';
-import { MatCheckbox, MatCheckboxModule } from '@angular/material/checkbox';
-import { UsersService } from '@api/services/users.service';
-import { debounceTime, distinctUntilChanged, Subject, tap } from 'rxjs';
-import { UpdateUserRequest } from '@api/models/updateUserRequest';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { SuccessSnackbarComponent } from '@shared/components/snackbar/success-snackbar/success-snackbar.component';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { UntilDestroy } from '@ngneat/until-destroy';
+import { UserProfileComponent } from '@shared/components/user-profile/user-profile.component';
+import { AuthService } from '@core/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -25,76 +20,22 @@ import { UntilDestroy } from '@ngneat/until-destroy';
     MatIconModule,
     MatButtonModule,
     RouterLinkActive,
-    MatDivider,
     NgOptimizedImage,
     FormsModule,
-    MatCheckbox,
-    MatCheckboxModule
+    MatCheckboxModule,
+    UserProfileComponent
   ],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 @UntilDestroy({ checkProperties: true })
 export class HeaderComponent {
-  readonly user = this.authService.user;
-  enableEmailNotifications: boolean | undefined;
-  email: string | undefined;
 
-  emailChanged$ = new Subject<string>();
-  enableEmailNotificationsChanged$ = new Subject<boolean>();
-
-  constructor(private authService: AuthService, private userService: UsersService, private snackBar: MatSnackBar) {
+  get isAuthenticated() {
+    return !!this.authService.token;
   }
 
-  getUserInfo() {
-    if (!this.user) return;
-
-    this.userService.usersUserIdGet(this.user.user_id)
-      .pipe(
-        tap((user) => {
-          this.enableEmailNotifications = user.enableEmailNotifications;
-          this.email = user.email!;
-        })
-      )
-      .subscribe();
-
-    this.emailChanged$
-      .pipe(
-        debounceTime(500),
-        distinctUntilChanged()
-      )
-      .subscribe(newEmail => this.updateUser(newEmail, this.enableEmailNotifications!));
-
-    this.enableEmailNotificationsChanged$
-      .pipe(
-        debounceTime(250),
-        distinctUntilChanged()
-      )
-      .subscribe(newValue => this.updateUser(this.email!, newValue));
-  }
-
-  onEmailChange(newValue: string) {
-    this.emailChanged$.next(newValue);
-  }
-
-  onEmailNotificationsChange(newValue: boolean) {
-    this.enableEmailNotificationsChanged$.next(newValue);
-  }
-
-  updateUser(email: string, enableEmailNotifications: boolean) {
-    const request = { email, enableEmailNotifications } as UpdateUserRequest;
-
-    this.userService.usersUserIdPut(this.user!.user_id, request).subscribe({
-      next: () => {
-        this.snackBar.openFromComponent(SuccessSnackbarComponent, {
-          data: { message: 'Настройки профиля успешно изменены!' }
-        });
-      }
-    });
-  }
-
-  logout(): void {
-    this.authService.logout();
+  constructor(private authService: AuthService) {
   }
 }
 
---
+++ src/app/shared/components/user-profile/user-profile.component.ts
@@ -0,0 +1,98 @@
+import { Component, OnInit } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatCheckbox } from '@angular/material/checkbox';
+import { MatDivider } from '@angular/material/divider';
+import { MatIcon } from '@angular/material/icon';
+import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
+import { NgIf, NgOptimizedImage } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, Subject, Subscription, tap } from 'rxjs';
+import { AuthService } from '@core/auth.service';
+import { UsersService } from '@api/services/users.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { UpdateUserRequest } from '@api/models/updateUserRequest';
+import { SuccessSnackbarComponent } from '@shared/components/snackbar/success-snackbar/success-snackbar.component';
+
+@Component({
+  selector: 'app-user-profile',
+  standalone: true,
+  imports: [
+    MatButton,
+    MatCheckbox,
+    MatDivider,
+    MatIcon,
+    MatMenu,
+    MatMenuItem,
+    NgIf,
+    NgOptimizedImage,
+    ReactiveFormsModule,
+    MatMenuTrigger,
+    FormsModule
+  ],
+  templateUrl: './user-profile.component.html',
+  styleUrl: './user-profile.component.scss'
+})
+export class UserProfileComponent implements OnInit {
+  readonly user = this.authService.user;
+  enableEmailNotifications: boolean | undefined;
+  email: string | undefined;
+
+  emailChanged$ = new Subject<string>();
+  emailChangedSub: Subscription | undefined;
+  enableEmailNotificationsChanged$ = new Subject<boolean>();
+  enableEmailNotificationsSub: Subscription | undefined;
+
+  constructor(private authService: AuthService, private userService: UsersService, private snackBar: MatSnackBar) {
+  }
+
+  ngOnInit(): void {
+    if (!this.user) return;
+
+    this.userService.usersUserIdGet(this.user.user_id)
+      .pipe(
+        tap((user) => {
+          this.enableEmailNotifications = user.enableEmailNotifications;
+          this.email = user.email!;
+        })
+      )
+      .subscribe();
+
+    this.emailChangedSub = this.emailChanged$
+      .pipe(
+        debounceTime(500),
+        distinctUntilChanged()
+      )
+      .subscribe(newEmail => this.updateUser(newEmail, this.enableEmailNotifications!));
+
+    this.enableEmailNotificationsSub = this.enableEmailNotificationsChanged$
+      .pipe(
+        debounceTime(250),
+        distinctUntilChanged()
+      )
+      .subscribe(newValue => this.updateUser(this.email!, newValue));
+  }
+
+  onEmailChange(newValue: string) {
+    this.emailChanged$.next(newValue);
+  }
+
+  onEmailNotificationsChange(newValue: boolean) {
+    this.enableEmailNotificationsChanged$.next(newValue);
+  }
+
+  updateUser(email: string, enableEmailNotifications: boolean) {
+    const request = { email, enableEmailNotifications } as UpdateUserRequest;
+
+    this.userService.usersUserIdPut(this.user!.user_id, request).subscribe({
+      next: () => {
+        this.snackBar.openFromComponent(SuccessSnackbarComponent, {
+          data: { message: 'Настройки профиля успешно изменены!' }
+        });
+      }
+    });
+  }
+
+  logout(): void {
+    this.authService.logout();
+  }
+}
